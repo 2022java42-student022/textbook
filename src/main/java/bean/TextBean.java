4ad64a21166ff6420001aa07df0e09f10b1cdd45
三澤 <2022java42-student021@la-study.com>
2022-05-19 12:29:38 +0900

    bean作成

--- src/main/java/bean/TextBean.java
+++ src/main/java/bean/TextBean.java
@@ -1,5 +1,56 @@
 package bean;
 
 public class TextBean {
-
+	
+	private int ISBN;
+	private String title;
+	private String sort;
+	private String author;
+	private int price;
+	private String use;
+	private int total;
+	
+	
+	public int getISBN() {
+		return ISBN;
+	}
+	public void setISBN(int iSBN) {
+		ISBN = iSBN;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSort() {
+		return sort;
+	}
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getUse() {
+		return use;
+	}
+	public void setUse(String use) {
+		this.use = use;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
 }
