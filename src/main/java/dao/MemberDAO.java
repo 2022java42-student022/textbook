225d49b5b2675607c8d6c7d125b1b7b6bcc7c530
Igadesu <2022java42-student022@la-study.com>
2022-05-24 10:45:25 +0900

    会員だお

--- src/main/java/dao/MemberDAO.java
+++ src/main/java/dao/MemberDAO.java
@@ -140,5 +140,30 @@ public class MemberDAO {
 
 		}
 
+
+	}
+
+	public int searchByEmail(String email) throws DAOException {
+		int user_id = 0;
+		String sql = "SELECT * FROM member WHERE email=?";
+		try (Connection con = DriverManager.getConnection(url, user, passwd);
+				PreparedStatement st = con.prepareStatement(sql);) {
+			st.setString(1, email);
+			try (ResultSet rs = st.executeQuery();) {
+				while (rs.next()) {
+					user_id = rs.getInt(user_id);
+				}
+				return user_id;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new DAOException("レコードの取得に失敗しました。");
+			}
+
+			// いっちせんとあかん
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの取得に失敗しました。");
+		}
+
 	}
 }
